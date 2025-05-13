f2f40e9afb6264f87b034c8c0b550bd91afa0447
Haseena C L <haseenalatheef807@gmail.com>
2025-05-13 12:03:27 +0530

    hh

---
+++ src/Details.js
@@ -0,0 +1,132 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.png";
+import logo from "./assets/logo2.png";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Haseena C L",
+  tagline: "I turn ideas into working web solutions.",
+  img: profile,
+  about: `I’m Haseena C.L., a Computer Science graduate with a strong passion for full stack web development. Currently pursuing an internship in this field, I’m constantly expanding my skill set and gaining hands-on experience in both frontend and backend technologies.
+
+My focus lies in building responsive, user-friendly websites using HTML, CSS, JavaScript, and React, while also working with backend tools like Python Flask and MySQL. I love transforming ideas into clean, functional web applications and enjoy learning new technologies that enhance my development workflow.
+
+I’m always looking for opportunities to grow and collaborate on exciting projects. Feel free to reach out if you'd like to connect or learn more!`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/haseena-c-l-320a32268/",
+  github: "https://github.com/haseenacl",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Full stack developer intern",
+    Company: `Edurevo`,
+    Location: "Kozhikode",
+    Type: "Intership",
+    Duration: "March-2025,Present",
+  },
+
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Technology in Computer Science & Engineering",
+    Company: "Government Model Engineering College",
+    Location: "Kochi",
+    Type: "Full Time",
+    Duration: "Sept 2022 - April 2025",
+  },
+  {
+    Position: "Diploma in computer engineering",
+    Company: `Government Womens Polytechnic College`,
+    Location: "Ernakulam",
+    Type: "Full Time",
+    Duration: "Aug 2020 - Mar 2022",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Architectural Plan Generation using Stable Diffusion Model",
+    image: projectImage1,
+    description: `Developed Architectural Plan Generation using Stable Diffusion Model , a website that generate architectural plan by integrating user preferences using HTML, CSS, JavaScript, Python Flask`,
+    techstack: "HTML/CSS, JavaScript ,Python Flask",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/haseenacl/Arch-AI",
+  },
+  {
+    title: "Home Service Management",
+    image: projectImage1,
+    description: `A platform to connect service providers and customers who want to avail their services.`,
+    techstack: "HTML,CSS,MySQl,PHP",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/haseenacl",
+  },
+  {
+    title: "Kiddo Shield",
+    image: projectImage1,
+    description: `Developed Kiddo Shield, a website dedicated to managing vaccination records for children aged 0-15 years old using HTML, CSS, JavaScript, Django`,
+    techstack: "HTML/CSS, JavaScript ,Django",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/haseenacl/kiddo-shield",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "haseenalatheef807@gmail.com",
+  phone: "+91 8078841257",
+};
